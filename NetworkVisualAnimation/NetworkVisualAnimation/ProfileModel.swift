9f2bdda0d42b300dfcb9daf59647a5c84885b934
Rinats-MBP <rinsdr@yandex.ru>
2023-08-31 12:54:38 +0300

    SwiftUINews removed, have no possibility to do this

---
+++ NetworkVisualAnimation/NetworkVisualAnimation/ProfileModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileModel.swift
+//  NetworkVisualAnimation
+//
+//  Created by Ринат on 29.08.2023.
+//
+
+import Foundation
---
+++ NetworkVisualAnimation/NetworkVisualAnimation/ProfileViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileViewController.swift
+//  NetworkVisualAnimation
+//
+//  Created by Ринат on 29.08.2023.
+//
+
+import Foundation
